a50254922db5d1c3416974b34a3bb6c6770636b3
Matheus C. Miranda <135903434+MatheusMiranda01@users.noreply.github.com>
2023-11-24 20:36:17 -0300

    aboutMe.py

---
+++ aboutMe.py
@@ -0,0 +1,16 @@
+print('--------Português----------------')
+print('👋 Olá, eu sou o Matheus')
+print(' 👀 Eu tenho interesse em adquirir mais conhecimento sobre as áreas de tecnologia e finalizar meu colegial.')
+print('🌱 Eu atualmente estou aprendendo front-end, back-end e pretendo estudar Ciência de Dados em breve.')
+print('💞️ Atualmente eu estou empregado como Jovem Aprendiz na empresa Martins ')
+print('📫 Entre em contato por aqui ou pelo meu LinkedIn')
+print('----------Inglês-----------------')
+print('👋 Hi, I’m @MatheusMiranda01')
+print('👀 I’m interested in get more knowledge over the technologies world, and end my high-school')
+print('🌱 I’m currently learning front-end, back-end, and hope to learn about data science soon')
+print('💞️Atualmente eu estou empregado como Jovem Aprendiz na empresa Martins')
+print('📫 How to reach me: LinkedIn')
+
+#MatheusMiranda01/MatheusMiranda01 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+#You can click the Preview link to take a look at your changes.
+
